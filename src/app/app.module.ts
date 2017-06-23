640b14c0e9d6343acc7d6d16c8b54f9e3ca373e7
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-06-23 14:01:07 +0530

    Added Spotify service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.routing';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -8,7 +10,7 @@ import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule ],
+  imports:      [ BrowserModule, AppRoutingModule, HttpModule, FormsModule ],
   declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service'
 
 @Component({
   moduleId: module.id,
   selector: 'search',
   templateUrl: `search.component.html`,
+  providers: [SpotifyService]
 })
-export class SearchComponent { }
+export class SearchComponent {
+	searchQuery: string;
+	constructor(private _spotifyService:SpotifyService) {}
+	searchMusic() {
+		this._spotifyService.searchMusic(this.searchQuery).subscribe(res => {
+			console.log(res.artists.items);
+		})
+	}
+}
