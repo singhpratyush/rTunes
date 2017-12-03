ce83070de30723aa3f4b909911719bda0ee1460f
hiteshpr <hiteshproy@gmail.com>
2017-12-03 22:47:58 +0530

    commited to new branch

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
+import { environment } from './../../environments/environment.js';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -8,25 +9,45 @@ export class SpotifyService{
 	private artistUrl: string;
 	private albumsUrl: string;
 	private albumUrl: string;
+	private countryUrl: string;
+	private tagUrl: string;
+	private tagArtistsUrl: string;
+
+	apiToken = environment.apiToken;
 
 	constructor(private _http:Http){}
 	searchArtist(str: string){
-		this.searchUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist='+str+'&api_key=73a3a0eaa1cc2d3de22e5b6450ed6652&format=json&limit=10';
+		this.searchUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist='+str+'&api_key='+this.apiToken+'&format=json&limit=10';
 		return this._http.get(this.searchUrl).map(res => res.json());
 	}
 
 	getArtistInfo(name: string){
-		this.artistUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+name+'&api_key=73a3a0eaa1cc2d3de22e5b6450ed6652&format=json';
+		this.artistUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+name+'&api_key='+this.apiToken+'&format=json';
 		return this._http.get(this.artistUrl).map(res => res.json());
 	}
 
 	getAlbums(name: string){
-		this.albumsUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist='+name+'&api_key=73a3a0eaa1cc2d3de22e5b6450ed6652&format=json&limit=10';
+		this.albumsUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist='+name+'&api_key='+this.apiToken+'&format=json&limit=10';
 		return this._http.get(this.albumsUrl).map(res => res.json());
 	}
 
 	getAlbumInfo(mbid: string){
-		this.albumUrl = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid='+mbid+'&api_key=73a3a0eaa1cc2d3de22e5b6450ed6652&format=json';
+		this.albumUrl = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid='+mbid+'&api_key='+this.apiToken+'&format=json';
 		return this._http.get(this.albumUrl).map(res => res.json());
+	}
+  
+	searchCountry(country: string){
+		this.countryUrl = 'http://		ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country='+country+'&api_key='+this.apiToken+'&format=json';
+		return this._http.get(this.countryUrl).map(res => res.json());
+	}
+
+	getTagInfo(tag: string){
+		this.tagUrl = 'http://ws.audioscrobbler.com/2.0/?method=tag.getinfo&tag='+tag+'&api_key='+this.apiToken+'&format=json';
+		return this._http.get(this.tagUrl).map(res => res.json());
+	}
+
+	getTagArtists(tag: string){
+		this.tagArtistsUrl = 'http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag='+tag+'&api_key='+this.apiToken+'&format=json&limit=10';
+		return this._http.get(this.tagArtistsUrl).map(res => res.json());
 	}
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiToken: '73a3a0eaa1cc2d3de22e5b6450ed6652',
+};
