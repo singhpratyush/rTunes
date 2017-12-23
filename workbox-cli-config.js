3fd5633de6cf8b3bef55fc90661bf8b07b9cc6fa
Hitesh Roy <hiteshproy@gmail.com>
2017-12-23 10:37:01 +0530

    Service-Worker workbox (#40)

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "src/",
+  "globPatterns": [
+    "**/*.{css,html,js,map,ts,png,ico,json}"
+  ],
+  "swDest": "src/sw-default.js",
+  "globIgnores": [
+    "../workbox-cli-config.js"
+  ]
+};
